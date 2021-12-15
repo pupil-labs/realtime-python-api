af8c1610cb3632f01be125dbfe5c83b5ec1f43a9
Pablo Prietz <pablo@prietz.org>
2021-12-15 16:22:23 +0100

    Require aiohttp[speedups]

---
+++ CHANGES.rst
@@ -0,0 +1,6 @@
+v0.0.2
+######
+- Require |aiohttp[speedups]|_
+
+.. |aiohttp[speedups]| replace:: ``aiohttp[speedups]``
+.. _aiohttp[speedups]: https://docs.aiohttp.org/en/stable/
--- src/pupil_labs/realtime_api/__init__.py
+++ src/pupil_labs/realtime_api/__init__.py
@@ -1,4 +1,4 @@
-"""Top-level entry-point for the <project_name> package"""
+"""pupil_labs.realtime_api"""
 
 # .version is generated on install via setuptools_scm, see pyproject.toml
 from .version import __version__, __version_info__
