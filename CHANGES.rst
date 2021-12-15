8031457dfbc193a7c06f996eccb9a95356bd108e
Pablo Prietz <pablo@prietz.org>
2021-12-15 16:23:00 +0100

    Implement pupil_labs.realtime_api.discovery.discover_devices

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,7 @@
 v0.0.2
 ######
 - Require |aiohttp[speedups]|_
+- Implement :py:func:`discover_devices <pupil_labs.realtime_api.discovery.discover_devices>`
 
 .. |aiohttp[speedups]| replace:: ``aiohttp[speedups]``
 .. _aiohttp[speedups]: https://docs.aiohttp.org/en/stable/
---
+++ src/pupil_labs/realtime_api/discovery.py
@@ -0,0 +1,73 @@
+import asyncio
+import contextlib
+import functools
+import logging
+import typing as T
+
+from zeroconf import ServiceStateChange
+from zeroconf.asyncio import AsyncZeroconf, AsyncServiceBrowser, AsyncServiceInfo
+
+logger = logging.getLogger(__name__)
+
+
+class Device(T.NamedTuple):
+    name: str
+    server: str
+    port: int
+    addresses: T.List[T.ByteString]
+
+
+async def discover_devices() -> T.Iterator[Device]:
+    logger.info("Searching for devices...")
+    async with AsyncZeroconf() as aiozeroconf:
+        queue = asyncio.Queue()
+        queue_fn = functools.partial(_queue_service_state_changes, queue)
+
+        browser = AsyncServiceBrowser(
+            aiozeroconf.zeroconf, "_http._tcp.local.", handlers=[queue_fn]
+        )
+        try:
+            while True:
+                yield await queue.get()
+        finally:
+            await browser.async_cancel()
+
+
+def is_valid_service_name(name: str) -> bool:
+    return name.split(":")[0] == "PI monitor"
+
+
+def _queue_service_state_changes(
+    queue, zeroconf, service_type: str, name: str, state_change: ServiceStateChange
+) -> None:
+    if is_valid_service_name(name) and state_change == ServiceStateChange.Added:
+        asyncio.create_task(
+            _request_info_for_valid_services_and_queue_result(
+                queue, zeroconf, service_type, name, timeout_ms=3000
+            )
+        )
+
+
+async def _request_info_for_valid_services_and_queue_result(
+    queue, zeroconf, service_type, name, timeout_ms
+):
+    info = AsyncServiceInfo(service_type, name)
+    await info.async_request(zeroconf, timeout_ms)
+    device = Device(
+        name,
+        info.server,
+        info.port,
+        ['.'.join([str(symbol) for symbol in addr]) for addr in info.addresses],
+    )
+    await queue.put(device)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    async def print_discovered_devices():
+        async for device in discover_devices():
+            print(device)
+
+    with contextlib.suppress(KeyboardInterrupt):
+        asyncio.run(print_discovered_devices())
