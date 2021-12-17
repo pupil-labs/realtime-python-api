c450d3c1539b5bb0e899125ebeaf047f1bf6c9e0
Pablo Prietz <pablo@prietz.org>
2021-12-17 13:19:56 +0100

    Expose Streamer classes

--- src/pupil_labs/realtime_api/__init__.py
+++ src/pupil_labs/realtime_api/__init__.py
@@ -1,9 +1,27 @@
 """pupil_labs.realtime_api"""
 
 # .version is generated on install via setuptools_scm, see pyproject.toml
-from .control import Control
+from .control import APIPath, Control, ControlError
 from .discovery import discover_devices
-from .streaming import GazeData, receive_gaze_data
+from .streaming import (
+    GazeData,
+    RTSPData,
+    RTSPGazeStreamer,
+    RTSPRawStreamer,
+    receive_gaze_data,
+    receive_raw_rtsp_data,
+)
 from .version import __version__, __version_info__
 
-__all__ = ["Control", "discover_devices", "GazeData", "receive_gaze_data"]
+__all__ = [
+    "APIPath",
+    "Control",
+    "ControlError",
+    "discover_devices",
+    "GazeData",
+    "receive_gaze_data",
+    "receive_raw_rtsp_data",
+    "RTSPData",
+    "RTSPGazeStreamer",
+    "RTSPRawStreamer",
+]
--- src/pupil_labs/realtime_api/streaming/__init__.py
+++ src/pupil_labs/realtime_api/streaming/__init__.py
@@ -1,3 +1,11 @@
-from .gaze import GazeData, receive_gaze_data
+from .base import RTSPData, RTSPRawStreamer, receive_raw_rtsp_data
+from .gaze import GazeData, RTSPGazeStreamer, receive_gaze_data
 
-__all__ = ["GazeData", "receive_gaze_data"]
+__all__ = [
+    "GazeData",
+    "receive_gaze_data",
+    "receive_raw_rtsp_data",
+    "RTSPData",
+    "RTSPGazeStreamer",
+    "RTSPRawStreamer",
+]
