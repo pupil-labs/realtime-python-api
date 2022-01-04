e8ae2c42396c1a5309960a5d517ad0d1b2d5a920
Pablo Prietz <pablo@prietz.org>
2022-01-04 09:46:09 +0100

    Auto-update basic.Device._status

--- examples/basic/get_status.py
+++ examples/basic/get_status.py
@@ -1,5 +1,9 @@
 from pupil_labs.realtime_api.basic import discover_one_device
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 # Look for devices. Returns as soon as it has found the first device.
 print("Looking for the next best device...")
 device = discover_one_device(max_search_duration_seconds=10)
@@ -7,17 +11,15 @@ if device is None:
     print("No device found.")
     raise SystemExit(-1)
 
-print("Getting status...")
-status = device.get_status()
+print(f"Device IP address: {device._status.phone.ip}")
+print(f"Battery level: {device._status.phone.battery_level} %")
 
-print(f"Device IP address: {status.phone.ip}")
-print(f"Battery level: {status.phone.battery_level} %")
+print(f"Connected glasses: SN {device._status.hardware.glasses_serial}")
+print(f"Connected scene camera: SN {device._status.hardware.world_camera_serial}")
 
-print(f"Connected glasses: SN {status.hardware.glasses_serial}")
-print(f"Connected scene camera: SN {status.hardware.world_camera_serial}")
-
-world = status.direct_world_sensor()
+world = device._status.direct_world_sensor()
 print(f"World sensor: connected={world.connected} url={world.url}")
 
-gaze = status.direct_gaze_sensor()
+gaze = device._status.direct_gaze_sensor()
 print(f"Gaze sensor: connected={gaze.connected} url={gaze.url}")
+device.close()
--- src/pupil_labs/realtime_api/basic.py
+++ src/pupil_labs/realtime_api/basic.py
@@ -1,22 +1,26 @@
 import asyncio
+import threading
 import typing as T
 
 from .base import DeviceBase
 from .device import Device as _DeviceAsync
 from .discovery import discover_devices as _discover_devices_async
-from .models import Event, Status
+from .models import Event, Status, DiscoveredDeviceInfo
 
 
 def discovered_devices(search_duration_seconds: float) -> T.List["Device"]:
     """Return all devices that could be found in the given search duration."""
 
-    async def _collect_devices():
+    async def _collect_device_information() -> T.List[DiscoveredDeviceInfo]:
         return [
-            Device.from_discovered_device(dev)
-            async for dev in _discover_devices_async(search_duration_seconds)
+            dev_info
+            async for dev_info in _discover_devices_async(search_duration_seconds)
         ]
 
-    return asyncio.run(_collect_devices())
+    return [
+        Device.from_discovered_device(dev_info)
+        for dev_info in asyncio.run(_collect_device_information())
+    ]
 
 
 def discover_one_device(
@@ -24,11 +28,13 @@ def discover_one_device(
 ) -> T.Optional["Device"]:
     """Search until one device is found."""
 
-    async def _return_first_device():
-        async for dev in _discover_devices_async(max_search_duration_seconds):
-            return Device.from_discovered_device(dev)
+    async def _return_first_device() -> DiscoveredDeviceInfo:
+        async for dev_info in _discover_devices_async(max_search_duration_seconds):
+            return dev_info
 
-    return asyncio.run(_return_first_device())
+    dev_info = asyncio.run(_return_first_device())
+    if dev_info is not None:
+        return Device.from_discovered_device(dev_info)
 
 
 class Device(DeviceBase):
@@ -39,19 +45,21 @@ class Device(DeviceBase):
 
     """
 
-    def get_status(self) -> Status:
-        """Request the device's current status.
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._status = self._get_status()
+        self._auto_update_thread = threading.Thread(
+            target=self._auto_update,
+            name=f"{self} auto-update thread",
+        )
+        self._auto_update_thread.start()
 
-        Wraps :py:meth:`pupil_labs.realtime_api.device.Device.get_status`
-
-        :raises pupil_labs.realtime_api.device.DeviceError: if the request fails
-        """
-
-        async def _get_status():
-            async with _DeviceAsync.convert_from(self) as control:
-                return await control.get_status()
-
-        return asyncio.run(_get_status())
+    def close(self) -> None:
+        self._should_close_flag.set()
+        self._auto_update_thread.join()
+    
+    def __del__(self):
+        self.close()
 
     def recording_start(self) -> str:
         """Wraps :py:meth:`pupil_labs.realtime_api.device.Device.recording_start`
@@ -116,3 +124,27 @@ class Device(DeviceBase):
                 return await control.send_event(event_name, event_timestamp_unix_ns)
 
         return asyncio.run(_send_event())
+
+    def _get_status(self) -> Status:
+        """Request the device's current status.
+
+        Wraps :py:meth:`pupil_labs.realtime_api.device.Device.get_status`
+
+        :raises pupil_labs.realtime_api.device.DeviceError: if the request fails
+        """
+
+        async def _get_status():
+            async with _DeviceAsync.convert_from(self) as control:
+                return await control.get_status()
+
+        return asyncio.run(_get_status())
+
+    def _auto_update(self):
+        async def _auto_update_until_closed():
+            async with _DeviceAsync.convert_from(self) as device:
+                self._should_close_flag = asyncio.Event()
+                await device.start_auto_update(self._status.update)
+                await self._should_close_flag.wait()
+                await device.stop_auto_update()
+
+        return asyncio.run(_auto_update_until_closed())
