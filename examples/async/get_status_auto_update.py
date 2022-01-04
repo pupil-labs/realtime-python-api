b745e0c09df35e630081f3c858804d2bb4ddb028
Pablo Prietz <pablo@prietz.org>
2022-01-04 10:27:53 +0100

    Fix async auto-update example

--- examples/async/get_status_auto_update.py
+++ examples/async/get_status_auto_update.py
@@ -4,14 +4,21 @@ import logging
 from pupil_labs.realtime_api import Device
 
 
-async def print_component(component):
+def print_component(component):
     print(component)
+
+
+async def async_callback_example(component):
+    await asyncio.sleep(0.1)
 
 
 async def main():
     async with Device("pi.local", 8080) as device:
         print("Starting auto-update")
-        await device.start_auto_update(update_callback=print_component)
+        await device.start_auto_update(
+            update_callback=print_component,
+            update_callback_async=async_callback_example,
+        )
         await asyncio.sleep(20.0)
         print("Stopping auto-update")
         await device.stop_auto_update()
