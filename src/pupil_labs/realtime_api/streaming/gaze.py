547c3ebcf702f36114dd15b55a8b447e870c3cab
Kamen Kirov <kirov@pointcontrols.com>
2024-03-13 13:02:01 +0200

    Add EyestateGazeData

--- src/pupil_labs/realtime_api/streaming/gaze.py
+++ src/pupil_labs/realtime_api/streaming/gaze.py
@@ -56,6 +56,38 @@ class DualMonocularGazeData(T.NamedTuple):
     def timestamp_unix_ns(self):
         return int(self.timestamp_unix_seconds * 1e9)
 
+class EyestateGazeData(T.NamedTuple):
+    x: float
+    y: float
+    worn: bool
+    pupil_diam_left: float
+    eyeball_center_left_x: float
+    eyeball_center_left_y: float
+    eyeball_center_left_z: float
+    optical_axis_left_x: float
+    optical_axis_left_y: float
+    optical_axis_left_z: float
+    pupil_diam_right: float
+    eyeball_center_right_x: float
+    eyeball_center_right_y: float
+    eyeball_center_right_z: float
+    optical_axis_right_x: float
+    optical_axis_right_y: float
+    optical_axis_right_z: float
+    timestamp_unix_seconds: float
+
+    @classmethod
+    def from_raw(cls, data: RTSPData) -> "EyestateGazeData":
+        x, y, worn, es0, es1, es2, es3, es4, es5, es6, es7, es8, es9, es10, es11, es12, es13 = struct.unpack("!ffBffffffffffffff", data.raw)
+        return cls(x, y, worn == 255, es0, es1, es2, es3, es4, es5, es6, es7, es8, es9, es10, es11, es12, es13, data.timestamp_unix_seconds)
+
+    @property
+    def datetime(self):
+        return datetime.datetime.fromtimestamp(self.timestamp_unix_seconds)
+
+    @property
+    def timestamp_unix_ns(self):
+        return int(self.timestamp_unix_seconds * 1e9)
 
 async def receive_gaze_data(
     url, *args, **kwargs
@@ -69,7 +101,7 @@ class RTSPGazeStreamer(RTSPRawStreamer):
     async def receive(
         self,
     ) -> T.AsyncIterator[T.Union[GazeData, DualMonocularGazeData]]:
-        data_class_by_raw_len = {9: GazeData, 17: DualMonocularGazeData}
+        data_class_by_raw_len = {9: GazeData, 17: DualMonocularGazeData, 65: EyestateGazeData}
         async for data in super().receive():
             try:
                 cls = data_class_by_raw_len[len(data.raw)]
