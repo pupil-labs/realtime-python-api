b7a4cf92462ea095ad6a43bf8e73a5385c41cb4a
Miguel García García <mikelgg93@gmail.com>
2025-04-07 16:29:42 +0200

    Fix mypy issues on nal_unit.py

--- src/pupil_labs/realtime_api/streaming/nal_unit.py
+++ src/pupil_labs/realtime_api/streaming/nal_unit.py
@@ -30,10 +30,10 @@ def extract_payload_from_nal_unit(unit: ByteString) -> ByteString:
     start_code = b"\x00\x00\x00\x01"
     offset = 0
     # slice to keep ByteString type; indexing would return int in native byte order
-    first_byte = unit[:1]
+    first_byte_raw = unit[:1]
     # ensure network order for conversion to uint8
-    first_byte = struct.unpack("!B", first_byte)[0]
-    is_first_bit_one = first_byte & 0b10000000
+    first_byte = struct.unpack("!B", first_byte_raw)[0]
+    is_first_bit_one = (first_byte & 0b10000000) != 0
     if is_first_bit_one:
         # See section 1.3 of https://www.ietf.org/rfc/rfc3984.txt
         raise ValueError("First bit must be zero (forbidden_zero_bit)")
@@ -43,11 +43,11 @@ def extract_payload_from_nal_unit(unit: ByteString) -> ByteString:
         # Fragmentation unit FU-A
         # https://www.ietf.org/rfc/rfc3984.txt
         # Section 5.8.
-        fu_header = unit[1:2]  # get second byte while retaining ByteString type
-        fu_header = struct.unpack("!B", fu_header)[0]
+        fu_header_raw = unit[1:2]  # get second byte while retaining ByteString type
+        fu_header = struct.unpack("!B", fu_header_raw)[0]
         offset = 2  # skip first two bytes
 
-        is_fu_start_bit_one = fu_header & 0b10000000
+        is_fu_start_bit_one = (fu_header & 0b10000000) != 0
         if is_fu_start_bit_one:
             # reconstruct header of a non-fragmented NAL unit
             first_byte_bits_1_to_3 = first_byte & 0b11100000
